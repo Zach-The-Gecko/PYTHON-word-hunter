db76f4abbe6e6251ba6f21ceb8048055c12e6fa1
Zach Geckler <Zach.The.Gecko@gmail.com>
2024-06-11 10:44:53 -0600

    Made it more readable... Hopefully I didn't put too many comments

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+# How to use:
+# use the command line to input the letters on the gameboard
+# from the top to bottom in rows. For example, the command
+# 'python main.py abcdefghijklmnop' would be used for a boggle
+# board looking like this
+#       a b c d
+#       e f g h
+#       i j k l
+#       m n o p
+
+import sys
 import math
 
 
 class SupLetter():
+    """
+    SupLetter (super letter) is a class that has a letter and position attribute
+    and has a function that will return the letters adjacent to the super letter
+    """
 
     def __init__(self, letter, position):
         self.letter = letter
@@ -47,8 +62,8 @@ class SupLetter():
         for difference in adjacent_letters_index_differences:
             adjacent_super_letter = game_board[self.position + difference]
 
-            # Catches duplicates where adjacent letters repeats, otherwise
-            # It just creates a new array
+            # Condenses the letters so each letter is a key and
+            # nothing gets overwritten
             if adjacent_super_letter.letter in adjacent_letters:
                 adjacent_letters[adjacent_super_letter.letter].append(
                     (adjacent_super_letter.position, difference))
@@ -60,6 +75,12 @@ class SupLetter():
 
 
 class Pathway:
+    """
+    has attributes of previous positions (history) and a list
+    of directions (path). has a function that generates a new
+    history and path for adjacent letters on the superboard
+    """
+
     def __init__(self, history, path):
         self.history = history
         self.path = path
@@ -74,9 +95,10 @@ class Pathway:
             children = []
             for position, direction in adjacent_letters[letter]:
 
+                # Checks if the letter has already been used in the history
                 if not position in self.history:
-                    history = self.history[::]
-                    path = self.path[::]
+                    history = self.history[:]
+                    path = self.path[:]
 
                     history.append(position)
                     path.append(direction)
@@ -88,40 +110,60 @@ class Pathway:
 
 
 def does_word_work(word, sup_game_board):
+    """
+    accepts a word and the game board, returns a path object
+    if the word can be used in a word hunt game, otherwise
+    returns false
+    """
+    # The following code loops through the gameboard and tests to see
+    # if the first letter of the word matches any of the letters
+    # if it does, a new Pathway object is created and added to the
+    # pathways array
     pathways = []
-    for index in range(0, len(sup_game_board)):
-        if word[0] == sup_game_board[index].letter:
-            pathways.append(Pathway([index], []))
+    for sup_letter in sup_game_board:
+        if word[0] == sup_letter.letter:
+            pathways.append(Pathway([sup_letter.position], []))
 
-    for letter_index in range(0, len(word)):
-        new_pathways = []
+    # Loops through the indexes of the word we're testing, checks
+    # if we are at the end of the word, and then for each
+    # letter it uses the pathways initially generated in the previous
+    # loop to get the letters adjacent to the letter we are currently on
+    # and stores the data in a new array called current_pathways.
+    for current_letter_index in range(0, (length_of_word := len(word))):
+        current_pathways = []
         for pathway in pathways:
-            if letter_index < len(word) - 1:
-                if (new_paths := pathway.get_next_layer_deep(sup_game_board, word[letter_index+1])):
-                    for new_path in new_paths:
-                        new_pathways.append(Pathway(new_path[0], new_path[1]))
+            if current_letter_index < length_of_word - 1:
+                if (branches := pathway.get_next_layer_deep(sup_game_board, word[current_letter_index + 1])):
+                    for branch in branches:
+                        current_pathways.append(Pathway(branch[0], branch[1]))
 
-        if not new_pathways:
-            if letter_index == len(word)-1:
+        # if there are no more pathways for the word to follow, that either
+        # means that we are at the end of the word, or that we could not complete
+        # the word. if we are at the end of the word we add the word to the pathway
+        # object and return the path as the result, otherwise we return false
+        if not current_pathways:
+            if current_letter_index == length_of_word - 1:
                 pathways[0].word = word
                 return pathways[0]
             return False
 
-        pathways = new_pathways[::]
+        # Sets the newly generated form of pathways to be the next set of pathways
+        # that my script will iterate over
+        pathways = current_pathways[:]
 
 
-dictionary = open("dictionary.txt", "r").read().lower().split("\n")
+user_input = sys.argv[1]
 
-user_input = "jdoeuvnmakfioshj"
+dictionary_arr = open("dictionary.txt", "r").read().lower().split("\n")
 
+# Genereates a gameboard where instead of letters, 'super letters' are used
 game_board = []
-
 for index, letter in enumerate(user_input):
     game_board.append(SupLetter(letter, index))
 
 valid_words = []
 
-for word in dictionary:
+for word in dictionary_arr:
     if (result := does_word_work(word, game_board)):
         valid_words.append(result.word)
 
